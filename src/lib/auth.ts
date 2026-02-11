99077a0c0bea745e20b89fbb2b79c18ca4813785
Mohammad Sumon <sumonsbgc@gmail.com>
2026-02-12 05:48:34 +0600

    solving the auth issue

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -11,7 +11,7 @@ export const auth = betterAuth({
 	secret: config.betterAuthSecret,
 	trustedOrigins: [config.appUrl, "http://localhost:3000"],
 	advanced: {
-		// cookiePrefix: "better-auth",
+		cookiePrefix: "better-auth",
 		useSecureCookies: false,
 	},
 	emailAndPassword: {
