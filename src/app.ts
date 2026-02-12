1ef0f5c2b956bd0fc5e6d9cdfa2b721bf880b4e7
Mohammad Sumon <sumonsbgc@gmail.com>
2026-02-12 07:16:16 +0600

    added cookie

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,7 @@ app.use(
 		credentials: true,
 		methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
 		allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
-		// exposedHeaders: ["Set-Cookie"],
+		exposedHeaders: ["Set-Cookie"],
 	}),
 );
 
